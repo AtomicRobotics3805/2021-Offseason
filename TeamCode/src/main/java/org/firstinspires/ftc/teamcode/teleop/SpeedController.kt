a3de20f3353fe163a2d80851a9b0f15027be9f8a
Adam Meilicke <meiliada000@edmonds15.org>
2020-12-25 12:29:52 -0800

    Created two classes, SpeedController.kt and TeleOpConstants.kt, to further object orient the code. Both are required to use the BasicTeleOp file that was added in the previous commit.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SpeedController.kt
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.teleop
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.qualcomm.robotcore.hardware.Gamepad
+
+class SlowModeController {
+    val SPEEDS = listOf(1.0, 0.5)
+
+    val mode = 0
+
+    fun update: Pose2d () {
+        
+    }
+}
