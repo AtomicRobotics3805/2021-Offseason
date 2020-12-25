8dd3e7b059675e01b37fff1743a2165d0f364ee9
Adam Meilicke <meiliada000@edmonds15.org>
2020-12-25 12:33:17 -0800

    Changed the name of OpMode to CompTeleOp and updated it to use BasicTeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CompTeleOp.kt
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+import org.firstinspires.ftc.teamcode.tuning.SampleMecanumDrive
+
+@TeleOp(name = "Competition OpMode")
+class CompTeleOp : BasicTeleOp() {
+
+    @Throws(InterruptedException::class)
+    override fun runOpMode() {
+        val drive = SampleMecanumDrive(hardwareMap)
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+        waitForStart()
+
+        while (opModeIsActive()) {
+            driveMotors()
+            telemetryPosition()
+        }
+    }
+}
