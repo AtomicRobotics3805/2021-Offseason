a3ec6acc72740908df170a1f76b0ba383c7d26b1
Adam Meilicke <meiliada000@edmonds15.org>
2020-12-25 10:02:47 -0800

    Converted TeleOp OpMode from Java to Kotlin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OpMode.kt
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.teleop
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.DcMotor
+import org.firstinspires.ftc.teamcode.tuning.SampleMecanumDrive
+
+class OpMode : LinearOpMode() {
+    @Throws(InterruptedException::class)
+    override fun runOpMode() {
+        val drive = SampleMecanumDrive(hardwareMap)
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+        waitForStart()
+        while (!isStopRequested) {
+            drive.setWeightedDrivePower(
+                    Pose2d(
+                            (-gamepad1.left_stick_y).toDouble(),
+                            (-gamepad1.left_stick_x).toDouble(),
+                            (-gamepad1.right_stick_x).toDouble()
+                    )
+            )
+            drive.update()
+            val poseEstimate = drive.poseEstimate
+            telemetry.addData("x", poseEstimate.x)
+            telemetry.addData("y", poseEstimate.y)
+            telemetry.addData("heading", poseEstimate.heading)
+            telemetry.update()
+        }
+    }
+}
