3d58284b4f09dd2d331b178508b09d3ac6f197f1
Adam Meilicke <meiliada000@edmonds15.org>
2020-12-25 12:27:55 -0800

    Created a Basic TeleOp class that can be used as a parent for most TeleOp OpModes and can be reused in future seasons. The class is dependent on Road Runner.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BasicTeleOp.kt
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.tuning.SampleMecanumDrive
+
+/*
+    Contains simple functionality that will likely be reused in future competitions
+    This includes driving and slow modes. It does NOT include contraption usage (arms, pulleys, etc.)
+ */
+
+@TeleOp(name = "Competition OpMode")
+abstract class BasicTeleOp : LinearOpMode() {
+
+    var drive: SampleMecanumDrive? = null
+
+    private val speedController = SpeedController(TeleOpConstants.speeds)
+
+    fun driveMotors() {
+        drive?.setWeightedDrivePower(speedController.drivePower)
+    }
+
+    fun telemetryPosition() {
+        val poseEstimate = drive?.poseEstimate
+        telemetry.addData("x", poseEstimate?.x ?: "Unknown")
+        telemetry.addData("y", poseEstimate?.y ?: "Unknown")
+        telemetry.addData("heading", poseEstimate?.heading ?: "Unknown")
+        telemetry.update()
+    }
+}
