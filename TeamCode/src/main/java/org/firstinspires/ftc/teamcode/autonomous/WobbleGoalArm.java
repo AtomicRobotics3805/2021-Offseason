d9f50434fe8e8586595f36be4cab5a806576bf46
Adam Meilicke <meiliada000@edmonds15.org>
2021-01-12 12:53:33 -0800

    Created placeholder Intake Shooter and Wobble Goal Arm classes for use in Autonomous program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/WobbleGoalArm.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import org.firstinspires.ftc.teamcode.hardware.compbot.CompMecanumDrive;
+
+public class WobbleGoalArm {
+
+    private CompMecanumDrive drive;
+
+    public WobbleGoalArm(CompMecanumDrive drive) {
+        this.drive = drive;
+    }
+
+    public void grabGoal() {
+
+    }
+
+    public void dropGoal() {
+
+    }
+}
