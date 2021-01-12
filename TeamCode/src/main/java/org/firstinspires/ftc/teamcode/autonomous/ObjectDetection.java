5dd0b56331f03cd50edc51254ebda57398069261
Adam Meilicke <meiliada000@edmonds15.org>
2021-01-12 12:38:23 -0800

    Created placeholder TensorFlow Object Detection class for use in Autonomous program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ObjectDetection.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import org.firstinspires.ftc.teamcode.hardware.compbot.CompMecanumDrive;
+
+public class ObjectDetection {
+    enum StackSize {
+        NONE,
+        ONE,
+        FOUR
+    }
+
+    public StackSize detect(CompMecanumDrive drive) {
+        StackSize stackSize = null;
+        return stackSize;
+    }
+}
