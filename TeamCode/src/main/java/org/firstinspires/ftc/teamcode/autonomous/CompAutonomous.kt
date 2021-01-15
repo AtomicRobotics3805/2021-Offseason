dbceb9424b57f1073b154a4c9871941729ea79bf
Adam Meilicke <meiliada000@edmonds15.org>
2021-01-14 17:35:23 -0800

    Created three paths and programmed CompAutonomous to follow them depending on ObjectDetection's detected stack size

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CompAutonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CompAutonomous.kt
@@ -1,29 +1,44 @@
 package org.firstinspires.ftc.teamcode.autonomous
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.hardware.compbot.CompMecanumDrive
 
 class CompAutonomous : LinearOpMode() {
-
-    private var drive: CompMecanumDrive? = null
-    private var mechanismController: MechanismController? = null
-
-    private var stackSize = ObjectDetection.StackSize.NONE;
-
+    private var stackSize = ObjectDetection.StackSize.NONE
 
     override fun runOpMode() {
-        drive = CompMecanumDrive(hardwareMap)
-        drive?.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+        telemetry.addLine("Initializing")
+        telemetry.update()
 
-        mechanismController = MechanismController(drive)
+        var drive = CompMecanumDrive(hardwareMap)
+        var mech = MechanismController(drive)
+        var pathManager = PathManager(drive, mech)
+        var runtime = ElapsedTime()
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+
+        pathManager.generatePaths()
 
         stackSize = ObjectDetection.detect(drive)
 
-        followPath()
-    }
+        telemetry.addLine("Ready")
+        telemetry.addData("Detected Stack Size", stackSize.name)
+        telemetry.update()
 
-    fun followPath() {
+        waitForStart()
 
+        runtime.reset()
+
+        telemetry.addLine("Running Path")
+        telemetry.update()
+
+        pathManager.followPath(stackSize)
+
+        telemetry.addLine("Path Complete")
+        telemetry.addData("Time Elapsed", runtime.seconds())
+        telemetry.update()
+
+        while(opModeIsActive()) { }
     }
 }
