9bcd800a087cd0d8e2cd61b31556fdd1d015f098
Adam Meilicke <meiliada000@edmonds15.org>
2021-01-21 16:35:12 -0800

    Named trajectories as opposed to storing them in lists and built them in the PathManager constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CompAutonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CompAutonomous.kt
@@ -2,27 +2,35 @@ package org.firstinspires.ftc.teamcode.autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.teamcode.autonomous.MechanismController
+import org.firstinspires.ftc.teamcode.autonomous.ObjectDetection
+import org.firstinspires.ftc.teamcode.autonomous.PathManager
 import org.firstinspires.ftc.teamcode.hardware.compbot.CompMecanumDrive
 
 class CompAutonomous : LinearOpMode() {
     private var stackSize = ObjectDetection.StackSize.NONE
 
+    private lateinit var drive: CompMecanumDrive
+    private lateinit var mech: MechanismController
+    private lateinit var pathManager: PathManager
+    private var runtime: ElapsedTime = ElapsedTime()
+
     override fun runOpMode() {
+        runtime.reset()
+
         telemetry.addLine("Initializing")
         telemetry.update()
 
-        var drive = CompMecanumDrive(hardwareMap)
-        var mech = MechanismController(drive)
-        var pathManager = PathManager(drive, mech)
-        var runtime = ElapsedTime()
+        drive = CompMecanumDrive(hardwareMap)
+        mech = MechanismController(drive)
+        pathManager = PathManager(drive, mech)
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
-
-        pathManager.generatePaths()
 
         stackSize = ObjectDetection.detect(drive)
 
         telemetry.addLine("Ready")
+        telemetry.addData("Time Elapsed", runtime.seconds())
         telemetry.addData("Detected Stack Size", stackSize.name)
         telemetry.update()
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PathManager.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PathManager.kt
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.autonomous
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
-import com.acmerobotics.roadrunner.trajectory.MarkerCallback
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import org.firstinspires.ftc.teamcode.hardware.compbot.CompMecanumDrive
 import org.firstinspires.ftc.teamcode.util.Vector2d
@@ -10,7 +9,7 @@ import org.firstinspires.ftc.teamcode.util.toRadians
 import org.firstinspires.ftc.teamcode.autonomous.ObjectDetection.*
 import kotlin.math.atan
 
-class PathManager (val drive: CompMecanumDrive, val mech: MechanismController) {
+class PathManager (private var drive: CompMecanumDrive, var mech: MechanismController) {
 
     private val startPose = Pose2d(-48.0, 48.0, 0.0.toRadians)
 
@@ -18,121 +17,69 @@ class PathManager (val drive: CompMecanumDrive, val mech: MechanismController) {
 
     private val towerPose = Vector2d(72.0,36.0)
 
-    private var trajectories = mutableListOf<MutableList<Trajectory>>(mutableListOf(), mutableListOf(), mutableListOf())
+    // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
+    private var startToLowToShootPowershot = drive.trajectoryBuilder(startPose, startPose.heading)
+            .splineToSplineHeading(Pose2d(8.0, 50.0, 140.0.toRadians), 320.0.toRadians)
+            .addDisplacementMarker{ mech.dropGoal() }
+            .splineToSplineHeading(Pose2d(-7.0, 28.0, powerShotAngle(Vector2d(-7.0, 28.0), 0)), 200.0.toRadians)
+            .build()
+    private var startToMidToShootPowershot = drive.trajectoryBuilder(startPose, startPose.heading)
+            .splineToSplineHeading(Pose2d(-30.0, 52.0, 0.0.toRadians), 30.0.toRadians)
+            .splineToSplineHeading(Pose2d(18.0, 38.0, 90.0.toRadians), 270.0.toRadians)
+            .addDisplacementMarker{ mech.dropGoal() }
+            .splineToSplineHeading(Pose2d(-7.0, 28.0, powerShotAngle(Vector2d(-7.0, 28.0), 0)), 160.0.toRadians)
+            .build()
+    private var startToHighToShootPowershot = drive.trajectoryBuilder(startPose, startPose.heading)
+            .splineToSplineHeading(Pose2d(-27.0, 52.0, 0.0.toRadians), 0.0.toRadians)
+            .splineToSplineHeading(Pose2d(42.0, 58.0, 90.0.toRadians), 350.0.toRadians)
+            .addDisplacementMarker{ mech.dropGoal() }
+            .splineToSplineHeading(Pose2d(-7.0, 28.0, powerShotAngle(Vector2d(-7.0, 28.0), 0)), 160.0.toRadians)
+            .build()
 
+    // travel to ring(s)
+    private var shootPowershotToRing: Trajectory = drive.trajectoryBuilder(startToMidToShootPowershot.end(), startToMidToShootPowershot.end().heading)
+            .lineToLinearHeading(Pose2d(-24.0, 36.0, 320.0.toRadians))
+            .build()
 
-    public fun generatePaths() {
-        // separate mechanism movements performed in follow methods
+    // travel to second wobble goal
+    private var shootPowershotToWobble = drive.trajectoryBuilder(startToLowToShootPowershot.end(), true)
+            .splineToSplineHeading(Pose2d(-48.0, 35.0, 350.0.toRadians), 180.0.toRadians)
+            .build()
+    private var ringToWobble = drive.trajectoryBuilder(shootPowershotToRing.end(), true)
+            .splineToSplineHeading(Pose2d(-48.0, 35.0, 350.0.toRadians), 200.0.toRadians)
+            .build()
 
-        // PATH A
-        // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
-        trajectories[0][0] = drive.trajectoryBuilder(startPose, startPose.heading)
-                .splineToSplineHeading(Pose2d(8.0, 50.0, 140.0.toRadians), 320.0.toRadians)
-                .addDisplacementMarker(MarkerCallback{ mech.dropGoal() })
-                .splineToSplineHeading(Pose2d(-7.0, 28.0, powerShotAngle(Vector2d(-7.0, 28.0), 0)), 200.0.toRadians)
-                .build()
+    // travel to drop zone, drop wobble goal between movements, park
+    private var wobbleToLowToPark = drive.trajectoryBuilder(shootPowershotToWobble.end(), shootPowershotToWobble.end().heading)
+            .splineToSplineHeading(Pose2d(8.0, 50.0, 140.0.toRadians), 330.0.toRadians)
+            .splineToSplineHeading(Pose2d(10.0, 28.0, 0.0.toRadians), 270.0.toRadians)
+            .build()
+    private var wobbleToMidToPark = drive.trajectoryBuilder(ringToWobble.end(), ringToWobble.end().heading)
+            .splineToSplineHeading(Pose2d(18.0, 38.0, 90.0.toRadians), 270.0.toRadians)
+            .addDisplacementMarker{ mech.dropGoal() }
+            .splineToSplineHeading(Pose2d(10.0, 28.0, 0.0.toRadians), 240.0.toRadians)
+            .build()
+    private var wobbleToHighToPark = drive.trajectoryBuilder(ringToWobble.end(), ringToWobble.end().heading)
+            .splineToSplineHeading(Pose2d(42.0, 58.0, 90.0.toRadians), 350.0.toRadians)
+            .addDisplacementMarker{ mech.dropGoal() }
+            .splineToSplineHeading(Pose2d(10.0, 28.0, 0.0.toRadians), 240.0.toRadians)
+            .build()
 
-        // shoot rings
-
-        // travel to second wobble goal
-        trajectories[0][1] = drive.trajectoryBuilder(trajectories[0][0].end(), true)
-                .splineToSplineHeading(Pose2d(-48.0, 35.0, 350.0.toRadians), 180.0.toRadians)
-                .build()
-
-        // pick up second wobble goal
-
-        // travel to drop zone, drop wobble goal between movements, park
-        trajectories[0][2] = drive.trajectoryBuilder(trajectories[0][1].end(), trajectories[0][1].end().heading)
-                .splineToSplineHeading(Pose2d(8.0, 50.0, 140.0.toRadians), 330.0.toRadians)
-                .splineToSplineHeading(Pose2d(10.0, 28.0, 0.0.toRadians), 270.0.toRadians)
-                .build()
-
-
-        // PATH B
-        // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
-        trajectories[1][0] = drive.trajectoryBuilder(startPose, startPose.heading)
-                .splineToSplineHeading(Pose2d(-30.0, 52.0, 0.0.toRadians), 30.0.toRadians)
-                .splineToSplineHeading(Pose2d(18.0, 38.0, 90.0.toRadians), 270.0.toRadians)
-                .addDisplacementMarker(MarkerCallback{ mech.dropGoal() })
-                .splineToSplineHeading(Pose2d(-7.0, 28.0, powerShotAngle(Vector2d(-7.0, 28.0), 0)), 160.0.toRadians)
-                .build()
-
-        // shoot rings
-
-        // travel to ring
-        trajectories[1][1] = drive.trajectoryBuilder(trajectories[1][0].end(), trajectories[1][0].end().heading)
-                .lineToLinearHeading(Pose2d(-24.0, 36.0, 320.0.toRadians))
-                .build()
-
-        // turn to tower
-
-        // shoot ring
-
-        // travel to second wobble goal
-        trajectories[1][2] = drive.trajectoryBuilder(trajectories[1][1].end(), true)
-                .splineToSplineHeading(Pose2d(-48.0, 35.0, 350.0.toRadians), 200.0.toRadians)
-                .build()
-
-        // pick up second wobble goal
-
-        // travel to drop zone, drop wobble goal between movements, park
-        trajectories[1][3] = drive.trajectoryBuilder(trajectories[1][2].end(), trajectories[1][2].end().heading)
-                .splineToSplineHeading(Pose2d(18.0, 38.0, 90.0.toRadians), 270.0.toRadians)
-                .addDisplacementMarker(MarkerCallback{ mech.dropGoal() })
-                .splineToSplineHeading(Pose2d(10.0, 28.0, 0.0.toRadians), 240.0.toRadians)
-                .build()
-
-
-        // PATH C
-        // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
-        trajectories[1][0] = drive.trajectoryBuilder(startPose, startPose.heading)
-                .splineToSplineHeading(Pose2d(-27.0, 52.0, 0.0.toRadians), 0.0.toRadians)
-                .splineToSplineHeading(Pose2d(42.0, 58.0, 90.0.toRadians), 350.0.toRadians)
-                .addDisplacementMarker(MarkerCallback{ mech.dropGoal() })
-                .splineToSplineHeading(Pose2d(-7.0, 28.0, powerShotAngle(Vector2d(-7.0, 28.0), 0)), 160.0.toRadians)
-                .build()
-
-        // shoot rings
-
-        // travel to ring
-        trajectories[1][1] = drive.trajectoryBuilder(trajectories[1][0].end(), trajectories[1][0].end().heading)
-                .lineToLinearHeading(Pose2d(-24.0, 36.0, 320.0.toRadians))
-                .build()
-
-        // turn to tower
-
-        // shoot ring
-
-        // travel to second wobble goal
-        trajectories[1][2] = drive.trajectoryBuilder(trajectories[1][1].end(), true)
-                .splineToSplineHeading(Pose2d(-48.0, 35.0, 350.0.toRadians), 200.0.toRadians)
-                .build()
-
-        // pick up second wobble goal
-
-        // travel to drop zone, drop wobble goal between movements, park
-        trajectories[1][3] = drive.trajectoryBuilder(trajectories[1][2].end(), trajectories[1][2].end().heading)
-                .splineToSplineHeading(Pose2d(42.0, 58.0, 90.0.toRadians), 350.0.toRadians)
-                .addDisplacementMarker(MarkerCallback{ mech.dropGoal() })
-                .splineToSplineHeading(Pose2d(10.0, 28.0, 0.0.toRadians), 240.0.toRadians)
-                .build()
-    }
-
-    public fun followPath(stackSize: StackSize) {
+    fun followPath(stackSize: StackSize) {
         mech.startIntake()
         when (stackSize) {
-            StackSize.NONE -> followPathA()
-            StackSize.ONE -> followPathB()
-            StackSize.FOUR -> followPathC()
+            StackSize.NONE -> followPathLow()
+            StackSize.ONE -> followPathMid()
+            StackSize.FOUR -> followPathHigh()
         }
         mech.stopIntake()
     }
 
-    private fun followPathA() {
+    private fun followPathLow() {
         // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
-        drive.followTrajectory(trajectories[0][0]);
+        drive.followTrajectory(startToLowToShootPowershot)
 
-        var pos = Vector2d(trajectories[0][0].end())
+        var pos = Vector2d(startToLowToShootPowershot.end())
 
         // shoot powershot 1
         mech.shootRing()
@@ -150,104 +97,100 @@ class PathManager (val drive: CompMecanumDrive, val mech: MechanismController) {
         mech.shootRing()
 
         // travel to second wobble goal
-        drive.followTrajectory(trajectories[0][1])
+        drive.followTrajectory(shootPowershotToWobble)
 
         // pick up second wobble goal
         mech.grabGoal()
 
         // travel to drop zone, drop wobble goal between movements, park
-        drive.followTrajectory(trajectories[0][2])
+        drive.followTrajectory(wobbleToLowToPark)
     }
 
-    private fun followPathB() {
+    private fun followPathMid() {
         // start intake
         mech.startIntake()
 
         // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
-        drive.followTrajectory(trajectories[1][0]);
-
-        var pos = Vector2d(trajectories[1][0].end())
+        drive.followTrajectory(startToMidToShootPowershot)
 
         // shoot powershot 1
         mech.shootRing()
 
         // turn to powershot 2
-        drive.turn(powerShotAngle(pos, 2))
+        drive.turn(powerShotAngle(Vector2d(startToMidToShootPowershot.end()), 2))
 
         // shoot powershot 2
         mech.shootRing()
 
         // turn to powershot 3
-        drive.turn(powerShotAngle(pos, 3))
+        drive.turn(powerShotAngle(Vector2d(startToMidToShootPowershot.end()), 3))
 
         // shoot powershot 3
         mech.shootRing()
 
         // travel to ring
-        drive.followTrajectory(trajectories[1][1])
+        drive.followTrajectory(shootPowershotToRing)
 
         // turn to tower
-        drive.turn(towerAngle(Vector2d(trajectories[1][1].end())))
+        drive.turn(towerAngle(Vector2d(shootPowershotToRing.end())))
 
-        // shoot ring
+        // shoot top goal
         mech.shootRing()
 
         // travel to second wobble goal
-        drive.followTrajectory(trajectories[1][2])
+        drive.followTrajectory(ringToWobble)
 
         // pick up second wobble goal
         mech.grabGoal()
 
         // travel to drop zone, drop wobble goal between movements, park
-        drive.followTrajectory(trajectories[1][3])
+        drive.followTrajectory(wobbleToMidToPark)
 
         // stop intake
         mech.stopIntake()
     }
 
-    private fun followPathC() {
+    private fun followPathHigh() {
         // start intake
         mech.startIntake()
 
         // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
-        drive.followTrajectory(trajectories[2][0]);
-
-        var pos = Vector2d(trajectories[2][0].end())
+        drive.followTrajectory(startToHighToShootPowershot)
 
         // shoot powershot 1
         mech.shootRing()
 
         // turn to powershot 2
-        drive.turn(powerShotAngle(pos, 2))
+        drive.turn(powerShotAngle(Vector2d(startToHighToShootPowershot.end()), 2))
 
         // shoot powershot 2
         mech.shootRing()
 
         // turn to powershot 3
-        drive.turn(powerShotAngle(pos, 3))
+        drive.turn(powerShotAngle(Vector2d(startToHighToShootPowershot.end()), 3))
 
         // shoot powershot 3
         mech.shootRing()
 
         // travel to ring
-        drive.followTrajectory(trajectories[2][1])
+        drive.followTrajectory(shootPowershotToRing)
 
         // turn to tower
-        drive.turn(towerAngle(Vector2d(trajectories[2][1].end())))
+        drive.turn(towerAngle(Vector2d(shootPowershotToRing.end())))
 
-        // shoot rings
+        // shoot top goal
         mech.shootRing(true)
         mech.shootRing(true)
         mech.shootRing()
 
         // travel to second wobble goal
-        drive.followTrajectory(trajectories[2][2])
+        drive.followTrajectory(ringToWobble)
 
         // pick up second wobble goal
         mech.grabGoal()
 
         // travel to drop zone, drop wobble goal between movements, park
-        drive.followTrajectory(trajectories[2][3])
+        drive.followTrajectory(wobbleToHighToPark)
 
         // stop intake
         mech.stopIntake()
