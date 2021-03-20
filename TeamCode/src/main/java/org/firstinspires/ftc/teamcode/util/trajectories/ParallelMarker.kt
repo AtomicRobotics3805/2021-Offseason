59b80c307ef58c8f869146c36e484ed745d450e6
Adam Meilicke <meiliada000@edmonds15.org>
2021-03-20 12:22:08 -0700

    WIP Added Parallel Markers that can be executed while following a trajectory

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/trajectories/ParallelMarker.kt
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.util.trajectories
+
+import org.firstinspires.ftc.teamcode.commands.AtomicCommand
+
+data class ParallelMarker(val time: Double, val command: AtomicCommand)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/trajectories/ParallelTrajectory.kt
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.util.trajectories
+
+import com.acmerobotics.roadrunner.trajectory.Trajectory
+
+class ParallelTrajectory(private val trajectory: Trajectory, private val markers: List<ParallelMarker>) {
+
+}
