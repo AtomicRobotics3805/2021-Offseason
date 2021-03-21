7c0a1dc2e7813a45e6dcd578757aa3d81e98ae9c
Adam Meilicke <meiliada000@edmonds15.org>
2021-03-20 20:25:31 -0700

    WIP Added partial auto routines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoRoutines.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoRoutines.kt
@@ -1,27 +1,68 @@
 package org.firstinspires.ftc.teamcode.autonomous
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
+import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import org.firstinspires.ftc.teamcode.commands.FollowTrajectory
-import org.firstinspires.ftc.teamcode.Singleton
-import org.firstinspires.ftc.teamcode.commands.Turn
-import org.firstinspires.ftc.teamcode.commands.sequential
-import org.firstinspires.ftc.teamcode.hardware.compbot.MecanumDriveComp
-import org.firstinspires.ftc.teamcode.util.toRadians
-import org.firstinspires.ftc.teamcode.util.trajectories.trajectoryBuilder
-import org.firstinspires.ftc.teamcode.util.trajectoryBuilder
+import org.firstinspires.ftc.teamcode.commands.*
+import org.firstinspires.ftc.teamcode.hardware.compbot.MechanismConstants.RING_DELAY
+import org.firstinspires.ftc.teamcode.hardware.compbot.Mechanisms.shooter
+import org.firstinspires.ftc.teamcode.hardware.compbot.Mechanisms.wobble
+import org.firstinspires.ftc.teamcode.trajectory.TrajectoryFactory
+import org.firstinspires.ftc.teamcode.util.Vector2d
 
 @Autonomous
-class AutoRoutines: LinearOpMode() {
+class AutoRoutines {
     private val startPose = Pose2d()
 
-    override fun runOpMode() {
-        Singleton.drive = MecanumDriveComp(hardwareMap, Singleton.constants)
-        sequential {
-            +FollowTrajectory(trajectoryBuilder(startPose).build())
-            +Turn(45.0.toRadians)
-            +FollowTrajectory(trajectoryBuilder(startPose).build())
+    val initRoutine: AtomicCommand
+        get() = sequential {
+            +parallel {
+                +wobble.raiseArm()
+                +wobble.closeClaw()
+            }
         }
-    }
+
+    val lowRoutine: AtomicCommand
+        get() = sequential {
+            +parallel {
+                +shooter.startMotor()
+                +FollowTrajectory(TrajectoryFactory.startToLowToShootPowershot)
+                +sequential {
+                    +Delay(0.8)
+                    +wobble.openClaw()
+                }
+            }
+            +shootPowershotRoutine
+            +parallel {
+                +shooter.stopMotor()
+                +FollowTrajectory(TrajectoryFactory.shootPowershotToWobble)
+                +wobble.lowerArm()
+            }
+            +wobble.closeClaw()
+            +parallel {
+                +FollowTrajectory(TrajectoryFactory.wobbleToLowToPark)
+            }
+        }
+
+    private val shootPowershotRoutine: AtomicCommand
+        get() = sequential {
+            +parallel {
+                +shooter.shootRing()
+                +sequential {
+                    +Delay(RING_DELAY / 2)
+                    +Turn(TrajectoryFactory.powerShotAngle(
+                            Vector2d(TrajectoryFactory.startToLowToShootPowershot.end()), 1))
+                }
+            }
+            +parallel {
+                +shooter.shootRing()
+                +sequential {
+                    +Delay(RING_DELAY / 2)
+                    +Turn(TrajectoryFactory.powerShotAngle(
+                            Vector2d(TrajectoryFactory.startToLowToShootPowershot.end()), 2))
+                }
+            }
+            +shooter.shootRing()
+        }
 }
