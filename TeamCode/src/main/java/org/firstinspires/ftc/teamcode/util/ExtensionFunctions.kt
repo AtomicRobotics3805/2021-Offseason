d05f0618e331edff9121300a5a65afc209b14a81
Adam Meilicke <meiliada000@edmonds15.org>
2021-04-15 12:18:54 -0700

    WIP Added OpMode.isStopRequested to easily use LinearOpMode.isStopRequested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ExtensionFunctions.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ExtensionFunctions.kt
@@ -2,9 +2,12 @@ package org.firstinspires.ftc.teamcode.util
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
 
 
 val Double.toRadians get() = (Math.toRadians(this))
 val Double.toDegrees get() = (Math.toDegrees(this))
+val OpMode.isStopRequested get() = (this is LinearOpMode && isStopRequested)
 
 fun Vector2d(pose: Pose2d) = Vector2d(pose.x, pose.y)
