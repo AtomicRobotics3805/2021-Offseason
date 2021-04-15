6aaf9b81b7556c5bcc97c6a1daff2a25d8b2d734
Adam Meilicke <meiliada000@edmonds15.org>
2021-04-15 12:19:52 -0700

    Changed Constants to use MecanumDriveComp object by default

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import org.firstinspires.ftc.teamcode.util.hardware.BaseDriveConstants
 import org.firstinspires.ftc.teamcode.util.hardware.BaseMecanumDrive
 import org.firstinspires.ftc.teamcode.hardware.compbot.DriveConstantsComp
+import org.firstinspires.ftc.teamcode.hardware.compbot.MecanumDriveComp
 import org.firstinspires.ftc.teamcode.subsystems.mechanisms.Intake
 import org.firstinspires.ftc.teamcode.subsystems.mechanisms.Shooter
 import org.firstinspires.ftc.teamcode.subsystems.mechanisms.Wobble
@@ -18,5 +19,6 @@ object Constants {
     var startPose = Pose2d()
     var color = Color.BLUE
     val constants: BaseDriveConstants = DriveConstantsComp
+    val drive: BaseMecanumDrive = MecanumDriveComp
     lateinit var opMode: OpMode
 }
