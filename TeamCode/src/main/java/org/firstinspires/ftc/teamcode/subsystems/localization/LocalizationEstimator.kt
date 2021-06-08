14d1eba1c56b8ce7cafa257727e7db22a8a9b823
Adam Meilicke <meiliada000@edmonds15.org>
2021-06-08 11:30:48 -0700

    Updated existing functions in LocalizationEstimator to match Kotlin and new variable names and starting adding more functions from Spartronics.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/localization/LocalizationEstimator.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/localization/LocalizationEstimator.kt
@@ -1,29 +1,35 @@
 package org.firstinspires.ftc.teamcode.subsystems.localization
 
-import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.localization.Localizer
-/*
 import edu.wpi.first.wpilibj.estimator.ExtendedKalmanFilter
 import edu.wpi.first.wpilibj.estimator.KalmanFilterLatencyCompensator
+import edu.wpi.first.wpilibj.geometry.Pose2d
 import edu.wpi.first.wpilibj.geometry.Rotation2d
+import edu.wpi.first.wpilibj.math.Discretization
+import edu.wpi.first.wpilibj.math.StateSpaceUtil
 import edu.wpi.first.wpiutil.math.Matrix
 import edu.wpi.first.wpiutil.math.Nat
+import edu.wpi.first.wpiutil.math.Num
 import edu.wpi.first.wpiutil.math.VecBuilder
 import edu.wpi.first.wpiutil.math.numbers.N1
 import edu.wpi.first.wpiutil.math.numbers.N3
 import edu.wpi.first.wpiutil.math.numbers.N5
 import edu.wpi.first.wpiutil.math.numbers.N6
- */
 import java.util.function.BiConsumer
+import java.util.function.BiFunction
+import com.acmerobotics.roadrunner.geometry.Pose2d as RoadRunnerPose2d
 
 
+/**
+ * @author FRC Team Spartronics4915
+ */
+/*
 object LocalizationEstimator : Localizer {
-    override var poseEstimate: Pose2d
+    override var poseEstimate: RoadRunnerPose2d
         get() = TODO("Not yet implemented")
         set(value) {}
-    override val poseVelocity: Pose2d?
+    override val poseVelocity: RoadRunnerPose2d?
         get() = TODO("Not yet implemented")
-    /*
 
     private lateinit var observer: ExtendedKalmanFilter<N5, N3, N6>
     private lateinit var visionCorrect: BiConsumer<Matrix<N3, N1>, Matrix<N3, N1>>
@@ -34,80 +40,64 @@ object LocalizationEstimator : Localizer {
 
     private lateinit var gyroOffset: Rotation2d
     private lateinit var previousAngle: Rotation2d
+    fun initialize(
+                   gyroAngle: Rotation2d, initialPoseMeters: Pose2d,
+                   stateStdDevs: Matrix<N5, N1>,
+                   localMeasurementStdDevs: Matrix<N6, N1>, visionMeasurementStdDevs: Matrix<N3, N1>,
+                   nominalDtSeconds: Double
+    ) {
+        nominalDt = nominalDtSeconds
 
-    fun initialize() {
-        
+        observer = ExtendedKalmanFilter(
+                Nat.N5(), Nat.N3(), Nat.N6(),
+                this::f,
+                BiFunction { x: Matrix<N5?, N1?>, u: Matrix<N3?, N1?>? ->
+                    VecBuilder.fill(
+                            x[0, 0], x[1, 0], x[2, 0],
+                            x[3, 0], x[4, 0],
+                            x[2, 0]
+                    )
+                },
+                stateStdDevs, localMeasurementStdDevs,
+                nominalDt
+        )
+        latencyCompensator = KalmanFilterLatencyCompensator<N5, N3, N6>()
+
+        val visionContR = StateSpaceUtil.makeCovarianceMatrix(Nat.N3(), visionMeasurementStdDevs)
+        val visionDiscR = Discretization.discretizeR(visionContR, nominalDt)
+        visionCorrect = BiConsumer { u: Matrix<N3, N1>, y: Matrix<N3, N1> ->
+            observer.correct(
+                    Nat.N3(), u, y,
+                    { x, _ -> Matrix<N3, N1>(x.storage.extractMatrix(0, 3, 0, 1)) },
+                    visionDiscR
+            )
+        }
+
+        gyroOffset = initialPoseMeters.rotation.rotateBy(gyroAngle.inverse())
+        previousAngle = initialPoseMeters.rotation
+        observer.setXhat(fillStateVector(initialPoseMeters, 0.0, 0.0))
     }
 
     fun addVisionMeasurement(visionRobotPoseMeters: Pose2d, timestampSeconds: Double) {
         val oldXHat = observer.xhat
         val oldP = observer.p
         val oldSnapshots = arrayListOf(latencyCompensator.)
-
-        // You're probably looking at this thinking "WTF is in that catch block"
-        // Well... There was a bug that I couldn't track down so I used all this code to dump the EKF state
-        // Hopefully I will have found the bug and fixed it by the time you're reading this, and then you can remove the stuff in the catch block
-        try {
-            m_latencyCompensator.applyPastGlobalMeasurement(
+            latencyCompensator.applyPastGlobalMeasurement(
                     Nat.N3(),
-                    m_observer, m_nominalDt,
+                    observer, nominalDt,
                     VecBuilder.fill(
                             visionRobotPoseMeters.getTranslation().getX(),
                             visionRobotPoseMeters.getTranslation().getY(),
                             visionRobotPoseMeters.getRotation().getRadians()
                     ),
-                    m_visionCorrect,
+                    visionCorrect,
                     timestampSeconds
             )
-        } catch (e: Exception) {
-            try {
-                PrintWriter(File("/home/lvuser/out.txt")).use { writer ->
-                    writer.println("y:")
-                    writer.println(VecBuilder.fill(
-                            visionRobotPoseMeters.getTranslation().getX(),
-                            visionRobotPoseMeters.getTranslation().getY(),
-                            visionRobotPoseMeters.getRotation().getRadians()
-                    ).toString())
-                    writer.println("Timestamp:")
-                    writer.println(timestampSeconds)
-                    writer.println("xHat old:")
-                    writer.println(oldXHat)
-                    writer.println("P old:")
-                    writer.println(oldP)
-                    writer.println("xHat:")
-                    writer.println(m_observer.getXhat())
-                    writer.println("P:")
-                    writer.println(m_observer.getP())
-                    writer.println("Snapshots old:")
-                    oldSnapshots.forEach { it ->
-                        val snap: `var` = it.getValue()
-                        writer.println(snap.xHat)
-                        writer.println(snap.errorCovariances)
-                        writer.println(snap.inputs)
-                        writer.println(snap.localMeasurements)
-                        writer.println("=======")
-                    }
-                    writer.println("Snapshots:")
-                    writer.println(m_latencyCompensator.m_pastObserverSnapshots.size())
-                    m_latencyCompensator.m_pastObserverSnapshots.forEach { it ->
-                        val snap: `var` = it.getValue()
-                        writer.println(snap.xHat)
-                        writer.println(snap.errorCovariances)
-                        writer.println(snap.inputs)
-                        writer.println(snap.localMeasurements)
-                        writer.println("=======")
-                    }
-                    writer.println("done")
-                    writer.flush()
-                }
-            } catch (ex: FileNotFoundException) {
-                ex.printStackTrace()
-            }
-        }
     }
-     */
 
     override fun update() {
         TODO("Not yet implemented")
     }
+
 }
+ */
